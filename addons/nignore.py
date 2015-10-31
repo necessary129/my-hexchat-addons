5eccf5c5a8f8fa7bcce255250d9e92db9002b7bd
Shamil K Muhammed <necessary129@users.noreply.github.com>
2015-10-31 10:12:25 +0530

    Initial Commit

---
+++ addons/nignore.py
@@ -0,0 +1,53 @@
+# Copyright (c) 2015 noteness
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE
+
+import hexchat
+__module_name__ = 'Nick Ignore'
+__module_version__ = '0.0.1'
+__module_description__ = 'Ignores nick changes.'
+__module_author__ = 'noteness'
+ignores = []
+
+def setignorer(word, word_eol, userdata):
+    global ignores
+    if len(word) < 2:
+        hexchat.prnt("Need a host maaan")
+    ignores.append(word[1])
+    hexchat.prnt('host {0} successfully added to ignore list'.format(word[1]))
+    return hexchat.EAT_NONE
+def on_nick(word, word_eol, userdata):
+    host =word[0].split('@')[1]
+    hexchat.prnt(str(word))
+    hexchat.prnt(str(word_eol))
+    hexchat.prnt(str(userdata))
+    if host in ignores:
+        return hexchat.EAT_ALL
+    return hexchat.EAT_NONE
+
+def unset(word, word_eol, userdata):
+    global ignores
+    if len(word) < 2:
+        hexchat.prnt("Need a host maaan")
+    if word[1] not in ignores:
+        hexchat.prnt('I am not ignoring that host')
+        return hexchat.EAT_NONE
+    ignores.remove(word[1])
+    hexchat.prnt('host {0} successfully removed from ignore list'.format(word[1]))
+    return hexchat.EAT_NONE
+hexchat.hook_server('NICK',on_nick,priority=hexchat.PRI_HIGHEST)
+hexchat.hook_command('NIGNORE',setignorer,help="/nignore <host>")
+hexchat.hook_command('UNNIGNORE',unset,help="/unnignore <host>")
---
+++ hexchat.py
@@ -0,0 +1,50 @@
+"""
+This script tests the addons.
+
+You *SHOULDN'T* load this on your HexChat. :)
+"""
+
+if __name__ == "__main__":
+    import os
+    import sys
+    broken = 0
+    print("HexChat addons test script.")
+    print("There might been shown weird stuff. Don't worry. :)\n")
+    for val in os.listdir("addons"):
+        val = val.replace(".py", "")
+        print("Testing {0}".format(val))
+        try:
+            __import__('addons.{0}'.format(val), globals=globals())
+            print("{0} is WORKING.".format(val))
+        except Exception as err:
+            print("{0} is FAILING. ({1})".format(val, err))
+            broken = 1
+    if broken == 1:
+        print("\nThere's broken addons. :(")
+        sys.exit(1)
+    else:
+        print("\nAll is fine. :)")
+        sys.exit(0)
+
+def hook_command(name, function, help):
+    function(help.split(" "), [help, help.split(" ")[1]], None)
+    
+def command(command):
+    split = command.replace("\x034", "").split(" ")
+    if split[0] == "say":
+        print("<testuser> {0}".format(command.replace(split[0] + " ", "")))
+    elif split[0] == "me":
+        print("* testuser {0}".format(command.replace(split[0] + " ", "")))
+    elif split[0] == "msg" or split[0] == "privmsg":
+        print(">{0}< {1}".format(split[1], command.replace(split[0] + " ", "").replace(split[1] + " ", "")))
+    elif split[0] == "notice":
+        print("->{0}<- {1}".format(split[1], command.replace(split[0] + " ", "").replace(split[1] + " ", "")))
+
+testword = [':Slavetator!noteness@unaffiliated/nessessary129/bot/slavetator', 'NICK', ':Slavetator___']
+testwordeol = [':Slavetator!noteness@unaffiliated/nessessary129/bot/slavetator NICK :Slavetator___', 'NICK :Slavetator___', ':Slavetator___']
+testdata = None
+def hook_server(raw,func,priority):
+    if raw == 'NICK':
+        func(testword,testwordeol,testdata)
+def prnt(stri):
+    print(stri)
